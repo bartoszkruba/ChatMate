60d97ed33c61e44f60768321ca45c38b7e425003
Bartosz Kruba <bartosz.kruba@gmail.com>
2019-02-20 10:10:30 +0100

    Code cleanup

--- src/client/clientApp/MessageInboxHandler.java
+++ src/client/clientApp/MessageInboxHandler.java
@@ -38,99 +38,36 @@ public class MessageInboxHandler extends Thread {
          Sendable s = messages.removeFirst();
          if (s instanceof Message) {
             Message m = (Message) s;
-            Platform.runLater(() -> {
-               switch (m.TYPE) {
-                  case CHANNEL_MESSAGE:
-                     processCHANNEL_MESSAGE(m);
-                     break;
-                  case JOIN_CHANNEL:
-                     processJOIN_CHANNEL(m);
-                     break;
-                  case LEAVE_CHANNEL: {
-                     {
-                        ArrayList<String> channel = Client.getInstance().getChannelMessages().get(m.CHANNEL);
-                        if (channel != null) {
-                           String message = "\n" + m.NICKNAME + " left";
-                           channel.add(message);
-                           Client.getInstance().channelUsers.get(m.CHANNEL).remove(new User("", m.SENDER));
-                           if (m.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-                              controller.users.remove(new User("", m.SENDER));
-                              controller.textArea.appendText(message);
-                           }
-                        }
-                        break;
-                     }
-                  }
-
-                  case DISCONNECT: {
-                     String message = "\n" + m.TEXT_CONTENT + " disconnected";
-                     Client.getInstance().getChannelMessages().get(m.CHANNEL).add(message);
-                     Client.getInstance().channelUsers.forEach((key, value) -> {
-                        value.remove(new User("", m.SENDER));
-                     });
-                     if (m.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
-                        controller.users.remove(new User("", m.SENDER));
-                        controller.textArea.appendText(message);
-                     }
-                     break;
-                  }
-                  case CONNECT: {
-                     Client.getInstance().setNickname(m.NICKNAME);
-                     Client.getInstance().setID(m.SENDER.toString());
-                  }
-                  case NICKNAME_CHANGE: {
-
-                     System.out.println("NICKNAME_CHANGE");
-                     System.out.println(m.NICKNAME);
-
-                     if (Client.getInstance().getID().equals(m.SENDER.toString())) {
-                        Client.getInstance().setNickname(m.NICKNAME);
-                     }
-
-                     User user = new User(m.NICKNAME, m.SENDER);
-                     int i = controller.users.indexOf(user);
-
-                     if (i >= 0) {
-                        controller.users.remove(i);
-                        controller.users.add(user);
-                     }
-
-                     Client.getInstance().channelUsers.values().forEach(v -> {
-                        v.remove(user);
-                        v.add(user);
-                     });
-
-                  }
+            switch (m.TYPE) {
+               case CHANNEL_MESSAGE:
+                  process_CHANNEL_MESSAGE(m);
+                  break;
+               case JOIN_CHANNEL:
+                  process_JOIN_CHANNEL(m);
+                  break;
+               case LEAVE_CHANNEL:
+                  process_LEAVE_CHANNEL(m);
+                  break;
+               case DISCONNECT:
+                  process_DISCONNECT(m);
+                  break;
+               case CONNECT: {
+                  process_CONNECT(m);
+                  break;
                }
-            });
+               case NICKNAME_CHANGE:
+                  process_NICKNAME_CHANGE(m);
+                  break;
+            }
          } else if (s instanceof Channel) {
             Channel channel = (Channel) s;
-
-            System.out.println(channel.getName());
-            channel.getUsers().forEach(u -> {
-               System.out.println(u.getNickName());
-            });
-
-            try {
-               Platform.runLater(() -> {
-                  controller.channels.add(channel);
-                  ArrayList<String> messages = Client.getInstance().getChannelMessages().getOrDefault(channel.getName(), new ArrayList<>());
-                  Client.getInstance().getChannelMessages().put(((Channel) s).getName(), messages);
-                  Client.getInstance().channelUsers.put(((Channel) s).getName(), new ConcurrentSkipListSet<>((channel).getUsers()));
-                  if (controller.channels.size() == 1) {
-                     controller.users.addAll((channel).getUsers());
-                     controller.channelList.getSelectionModel().selectFirst();
-                     Client.getInstance().setCurrentChannel((channel).getName());
-                  }
-               });
-            } catch (Exception e) {
-
-            }
+            process_CHANNEL(channel);
          }
       }
    }
 
-   private void processCHANNEL_MESSAGE(Message m) {
+
+   private void process_CHANNEL_MESSAGE(Message m) {
       ArrayList<String> channel = Client.getInstance().getChannelMessages().get(m.CHANNEL);
       if (channel != null) {
          String user;
@@ -152,7 +89,7 @@ public class MessageInboxHandler extends Thread {
       }
    }
 
-   private void processJOIN_CHANNEL(Message m) {
+   private void process_JOIN_CHANNEL(Message m) {
       Platform.runLater(() -> {
          ArrayList<String> channel = Client.getInstance().getChannelMessages().get(m.CHANNEL);
          if (channel != null) {
@@ -167,4 +104,77 @@ public class MessageInboxHandler extends Thread {
          }
       });
    }
+
+   private void process_LEAVE_CHANNEL(Message m) {
+      ArrayList<String> channel = Client.getInstance().getChannelMessages().get(m.CHANNEL);
+      if (channel != null) {
+         String message = "\n" + m.NICKNAME + " left";
+         channel.add(message);
+         Client.getInstance().channelUsers.get(m.CHANNEL).remove(new User("", m.SENDER));
+         if (m.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
+            controller.users.remove(new User("", m.SENDER));
+            controller.textArea.appendText(message);
+         }
+      }
+   }
+
+
+   private void process_DISCONNECT(Message m) {
+      String message = "\n" + m.TEXT_CONTENT + " disconnected";
+      Client.getInstance().getChannelMessages().get(m.CHANNEL).add(message);
+      Client.getInstance().channelUsers.forEach((key, value) -> {
+         value.remove(new User("", m.SENDER));
+      });
+      if (m.CHANNEL.equals(Client.getInstance().getCurrentChannel())) {
+         controller.users.remove(new User("", m.SENDER));
+         controller.textArea.appendText(message);
+      }
+   }
+
+   private void process_CONNECT(Message m) {
+      Client.getInstance().setNickname(m.NICKNAME);
+      Client.getInstance().setID(m.SENDER.toString());
+   }
+
+   private void process_NICKNAME_CHANGE(Message m) {
+      if (Client.getInstance().getID().equals(m.SENDER.toString())) {
+         Client.getInstance().setNickname(m.NICKNAME);
+      }
+
+      User user = new User(m.NICKNAME, m.SENDER);
+      int i = controller.users.indexOf(user);
+
+      if (i >= 0) {
+         Platform.runLater(() -> {
+            controller.users.remove(i);
+            controller.users.add(user);
+         });
+      }
+
+      Client.getInstance().channelUsers.values().forEach(v -> {
+         v.remove(user);
+         v.add(user);
+      });
+
+   }
+
+   private void process_CHANNEL(Channel channel) {
+
+      try {
+         Platform.runLater(() -> {
+            controller.channels.add(channel);
+            ArrayList<String> messages = Client.getInstance().getChannelMessages().getOrDefault(channel.getName(), new ArrayList<>());
+            Client.getInstance().getChannelMessages().put(channel.getName(), messages);
+            Client.getInstance().channelUsers.put(channel.getName(), new ConcurrentSkipListSet<>(channel.getUsers()));
+            if (controller.channels.size() == 1) {
+               controller.users.addAll(channel.getUsers());
+               controller.channelList.getSelectionModel().selectFirst();
+               Client.getInstance().setCurrentChannel(channel.getName());
+            }
+         });
+      } catch (Exception e) {
+
+      }
+   }
+
 }
